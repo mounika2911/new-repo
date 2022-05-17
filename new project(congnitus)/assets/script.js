c5c36667d58d0b49dc4e6f1235be2f8a58a1161e
mounika2911 <mouni.mani2911@gmail.com>
2022-05-17 16:13:12 -0400

    Add files via upload

---
+++ new project(congnitus)/assets/script.js
@@ -0,0 +1,8 @@
+
+const spanTransitions = document.querySelectorAll(".span-transitions")
+
+function displayText() {
+    spanTransitions.forEach(spanTransition =>{
+        spanTransition.style.display = "block"
+    })
+}
